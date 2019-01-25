3a59f639f913f4a21a62ed667eb61d0ad83d3d46
paulie4 <paulie4@gmail.com>
2019-01-24 23:37:37 -0500

    remove CSRF protection on index.html to allow POSTs w/ signed_request

--- hello/views.py
+++ hello/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Greeting
 
 # Create your views here.
+@csrf_exempt
 def index(request):
     # return HttpResponse('Hello from Python!')
     context = {'signedRequest': request.POST['signed_request:']}
